ce706b782c280dd472e03c50532c9e439b7f80c2
sreeraj-krishnan <thesreerajrulz@gmail.com>
2016-12-02 01:17:48 +0530

    add npm install .

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd redis-stable && make install
 
 redis-server.exe ./redis.conf
 redis-server.exe ./redis-slave.conf
-
+npm install .
 npm test
 node driver.js &
 echo "started server successfully @ http://localhost:8080, settings can be changed inside config/config.js"
