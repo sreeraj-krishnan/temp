64d8709df57b456c592bf6ac12ddc23a929c492e
sreeraj-krishnan <thesreerajrulz@gmail.com>
2016-12-02 17:33:45 +0530

    some fixes on GET to return early

--- driver.js
+++ driver.js
@@ -17,6 +17,7 @@ var cluster = require('cluster');
 
 if (cluster.isMaster) {
 	var cpuCount = require('os').cpus().length;
+	//console.log('workers : ' + cpuCount )
 	for (var i = 0; i < cpuCount; i += 1) 
 	{
         cluster.fork();
@@ -24,70 +25,67 @@ if (cluster.isMaster) {
 }
 else {
 	var client = redis.createClient(); //11111,'127.0.0.1'
-client.on('connect', function(){
-	//console.log('connected');
-});
-var read = redis.createClient(config.redis.readport,config.redis.host); //11111,'127.0.0.1'
-read.on('connect', function(){
-	//console.log('connected');
-});
+	client.on('connect', function(){
+		//console.log('connected');
+	});
+	var read = redis.createClient(config.redis.readport,config.redis.host); //11111,'127.0.0.1'
+	read.on('connect', function(){
+		//console.log('connected');
+	});
 
-
-var app = express();
+	var app = express();
 	
-
-app.put('/drivers/:driverid/locations', function(request, response)
-{
-  //console.time('processed');
-  var headers = request.headers;
-  var url = request.url;
-  var body = [];
-  var driverid = request.params.driverid;
-  var lower;
-  var upper;
-  var data;
+	app.put('/drivers/:driverid/locations', function(request, response)
+	{
+		//console.time('processed');
+		var headers = request.headers;
+		var url = request.url;
+		var body = [];
+		var driverid = request.params.driverid;
+		var lower;
+		var upper;
+		var data;
   
-  upper = (Number(driverid) <= Number(config.app.driverMaxid));
-  lower = (Number(driverid) > "0");
+		upper = (Number(driverid) <= Number(config.app.driverMaxid));
+		lower = (Number(driverid) > "0");
  
-  if( upper === false || lower === false )
-  {
-	response.statusCode = 404;
-	response.end('{}');	
-	return;
-  }
-  request.on('data', function(chunk) {
-		body.push(chunk);
-  }).on('end', function() {
-		body=  Buffer.concat(body).toString() ;
-		data = JSON.parse( body );
-		key=''
-		longitude = data['longitude'];
-		latitude = data['latitude'];
-		
-		if( longitude == undefined || longitude == null ||
-			latitude == undefined || latitude == null ||
-			validateGeoLocation( latitude.toString() +',' +longitude.toString()) == false )
+		if( upper === false || lower === false )
 		{
-			response.statusCode = 422;
-			response.end('{ "errors" : ["Invalid geo location"] }');
+			response.statusCode = 404;
+			response.end('{}');	
 			return;
-		}		
+		}
+		request.on('data', function(chunk) {
+			body.push(chunk);
+		}).on('end', function() {
+			body=  Buffer.concat(body).toString() ;
+			data = JSON.parse( body );
+			key=''
+			longitude = data['longitude'];
+			latitude = data['latitude'];
+			
+			if( longitude == undefined || longitude == null ||
+				latitude == undefined || latitude == null ||
+				validateGeoLocation( latitude.toString() +',' +longitude.toString()) == false )
+			{
+				response.statusCode = 422;
+				response.end('{ "errors" : ["Invalid geo location"] }');
+				return;
+			}		
+			
+			read.get(Number(driverid), function(err,reply){
+				if( err || reply == null ){ /*console.log('err : ' + err ); console.log( ' set : ' + reply);*/}
+				else
+				{	
+					key=getKeyFromString( reply, false );
+					client.lrem(key, 0,Number(driverid), function(err, reply){
+						//console.log('reply lrem : ' + reply);
+					});
+				}
+			});	
 		
-
-		read.get(Number(driverid), function(err,reply){
-			if( err || reply == null ){ /*console.log('err : ' + err ); console.log( ' set : ' + reply);*/}
-			else
-			{	
-				key=getKeyFromString( reply, false );
-				client.lrem(key, 0,Number(driverid), function(err, reply){
-					//console.log('reply lrem : ' + reply);
-				});
-				
-			}
-		});	
-		data['id'] = Number(driverid);
-		client.set(Number(driverid), JSON.stringify(data) , function(err,reply){
+			data['id'] = Number(driverid);
+			client.set(Number(driverid), JSON.stringify(data) , function(err,reply){
 				//console.log( ' set : ' + reply);
 				if( err || reply == null) { console.log('err : ' + err ); }
 				else {
@@ -99,32 +97,31 @@ app.put('/drivers/:driverid/locations', function(request, response)
 						if( err || reply == null ) { console.log( 'err: ' + err + ' for key ' + key ); }
 					});
 				}
-		});
+			});
 			
  
+		});
 	});
-});
 
-app.get('/drivers', function(request, response){
-  console.time("fetch");
-  var url = request.url;
-  var body = [];
-  var driverid;
-  var lower;
-  var upper;
-  var data;
-  //console.log(request.query.latitude + ' : ' + request.query.longitude + ' : ' + request.query.radius + ' : ' + request.query.limit);
-  if( request.query.longitude == undefined || request.query.longitude == null ||
-	  request.query.latitude == undefined  || request.query.latitude  == null ||
-	  validateGeoLocation( request.query.latitude.toString() +',' +request.query.longitude.toString()) == false 
-	)
-  {
-		response.statusCode = 400;
-		response.end('{ "errors" : ["Invalid geo location"] }');
-		//console.timeEnd("fetch");
-		return;
-  }	
-  {
+	app.get('/drivers', function(request, response){
+		//console.time("fetch");
+		var body = [];
+		var driverid;
+		var lower;
+		var upper;
+		var data;
+		//console.log(request.query.latitude + ' : ' + request.query.longitude + ' : ' + request.query.radius + ' : ' + request.query.limit);
+		if( request.query.longitude == undefined || request.query.longitude == null ||
+			request.query.latitude == undefined  || request.query.latitude  == null ||
+			validateGeoLocation( request.query.latitude.toString() +',' +request.query.longitude.toString()) == false 
+			)
+		{
+			response.statusCode = 400;
+			response.end('{ "errors" : ["Invalid geo location"] }');
+			//console.timeEnd("fetch");
+			return;
+		}	
+	{
 	    var limit = request.query.limit;
 		if( limit == undefined || limit == null )
 		{
@@ -152,32 +149,37 @@ app.get('/drivers', function(request, response){
 		{
 			Keys.push( getKeyFromString( nearbylocations[loc], true ) );
 		}
+		done = false;
 		doasync( Keys, qloc, limit, radius ).then( function(values){
-			
+			if( done ) return;
 			response.statusCode = 200;
 			response.end('[' + values.substring(0, values.length - 2) + ']');
-			console.timeEnd('fetch');
+			done = true;
+			//console.timeEnd('fetch');
 		});
-		
   }
 });
-app.listen(config.web.port); // defaulted to 8080
+
+	app.listen(config.web.port); // defaulted to 8080
 }
 
+cluster.on('exit', function (worker) {
+    console.log('Worker %d died :(', worker.id);
+    cluster.fork();
+});
 
 
 function doasync( allkeys , qloc, limits, radius )
 {
 	return new Promise( function (resolve,reject) {
-		var promises = [];
 		json=''
 		len = allkeys.length;
-		//console.log('len : ' + len)
+		ldone = false;
 		for( i=0; i < allkeys.length ; i++ )
 		{
 			var locations = getAllLocationsWithKey( allkeys[i],qloc, limits,radius);
-	
 			locations.then( function(values){
+				if( ldone ) return;
 				{
 					locs = values;
 					//console.log('locations : ' + locs[0])
@@ -188,7 +190,7 @@ function doasync( allkeys , qloc, limits, radius )
 							json += JSON.stringify(locs[1][i]) + ',\n'
 						}
 						resolve( json );
-						allkeys=[]; 
+						ldone = true;
 					}
 					else if( locs[0] > 0 )
 					{
@@ -200,15 +202,16 @@ function doasync( allkeys , qloc, limits, radius )
 					}
 					
 				}
-				//console.log('len : ' + len)
 				if( --len == 0 )
+				{
 					resolve(json);
+				}
 			});
 		}
 	});
 }
 
-function getAllLocationsWithKey( key,qloc,limits,radius )
+function getAllLocationsWithKey( key,qloc,limits,radius)
 {
 	return new Promise(function (resolve,reject ) {
 		read.lrange(key, 0, -1, function(err, replies){
